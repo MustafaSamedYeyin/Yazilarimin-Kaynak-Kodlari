cd1bdd808fd0e32f8c98720a056205036f79aac4
MustafaSamedYeyin <mustafasamedyeyin@gmail.com>
2021-07-26 13:59:00 +0300

    HashTest

---
+++ HashTest/HashTest.App/SaySomeThing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashTest.App
+{
+    public class SaySomeThing
+    {
+        public string SayHello(string name)
+        {
+            return "Hello " + name; 
+        }
+    }
+}
---
+++ HashTest/HashTest.Test/SaySomeThingTest.cs
@@ -0,0 +1,28 @@
+﻿using HashTest.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HashTest.Test
+{
+    public class SaySomeThingTest
+    {
+        [Fact]
+        public void SayHelloTest()
+        {
+            //Arrange 
+            string name = "Mustafa";
+            SaySomeThing saySomeThing = new SaySomeThing();
+
+            //Act
+            var Value = saySomeThing.SayHello(name);
+            //Assert
+
+            Assert.Equal("Hello Mustafa", Value);
+
+        }
+    }
+}
