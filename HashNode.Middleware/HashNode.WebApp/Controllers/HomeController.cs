7d1211af498a9e76e8f9c50b653676799c3fe6a2
MustafaSamedYeyin <mustafasamedyeyin@gmail.com>
2021-07-27 18:28:21 +0300

    Middleware Nedir

---
+++ HashNode.Middleware/HashNode.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HashNode.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+    }
+}
