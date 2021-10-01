5246d225f81bab40a31d82a351cb3e963101cd80
MustafaSamedYeyin <mustafayeyin@protonmail.com>
2021-10-01 14:18:39 +0300

    MustafaConfiguration adlı class oluşturuldu.

---
+++ DevConfiguration/Dev.to/Dev.to.Configuration.Part3.OptionsPattern/Config/MustafaConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dev.to.Configuration.Part3.OptionsPattern.Config
+{
+    public class MustafaConfiguration
+    {
+        public string Soyad { get; set; }
+        public string Yas { get; set; }
+    }
+}
