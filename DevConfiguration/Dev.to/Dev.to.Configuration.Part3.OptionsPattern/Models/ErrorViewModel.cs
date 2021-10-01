9ebda88db9d03d4f995b7169cab90caaff026019
MustafaSamedYeyin <mustafayeyin@protonmail.com>
2021-10-01 14:13:02 +0300

    Default mvc.

---
+++ DevConfiguration/Dev.to/Dev.to.Configuration.Part3.OptionsPattern/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dev.to.Configuration.Part3.OptionsPattern.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
